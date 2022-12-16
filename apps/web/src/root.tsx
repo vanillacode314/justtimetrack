a8b7280b6dfc75c9ea9e6207971a00ce84a51710
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-12-16 22:12:45 +0530

    update logo

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -6,6 +6,7 @@ import {
   FileRoutes,
   Head,
   Html,
+  Link,
   Meta,
   Routes,
   Scripts,
@@ -23,8 +24,13 @@ export const Root: Component = () => {
     <Html lang="en" data-theme="forest" class="font-sans">
       <Head>
         <Title>JustTimeTrack</Title>
+        <Meta
+          name="description"
+          content="A Free and Open source Time Tracker for Freelancers."
+        />
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link rel="icon" href="/assets/images/logo.png" />
       </Head>
       <Body>
         <Base>
