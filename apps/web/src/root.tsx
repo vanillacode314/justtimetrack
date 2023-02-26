24c9b34465d28dd5f1fdce4823f90fac8d82f483
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-02-26 19:35:29 +0530

    add meta provider

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -1,4 +1,5 @@
 // @refresh reload
+import { MetaProvider } from '@solidjs/meta'
 import { Component, Suspense } from 'solid-js'
 import {
   Body,
@@ -18,37 +19,39 @@ import { AddNewProjectModal } from '~/modals/AddNewProjectModal'
 
 export const Root: Component = () => {
   return (
-    <Html lang="en" data-theme="forest" class="font-sans">
-      <Head>
-        <Title>JustTimeTrack</Title>
-        <Meta
-          name="description"
-          content="A Free and Open source Time Tracker for Freelancers."
-        />
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Link rel="icon" href="/assets/images/logo.png" />
-      </Head>
-      <Body>
-        <Base>
-          <Suspense
-            fallback={
-              <div class="p-5 grid place-items-center">
-                <Spinner />
-              </div>
-            }
-          >
-            <ErrorBoundary>
-              <AddNewProjectModal />
-              <Routes>
-                <FileRoutes />
-              </Routes>
-            </ErrorBoundary>
-          </Suspense>
-          <Scripts />
-        </Base>
-      </Body>
-    </Html>
+    <MetaProvider tags={[]}>
+      <Html lang="en" data-theme="forest" class="font-sans">
+        <Head>
+          <Title>JustTimeTrack</Title>
+          <Meta
+            name="description"
+            content="A Free and Open source Time Tracker for Freelancers."
+          />
+          <Meta charset="utf-8" />
+          <Meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Link rel="icon" href="/assets/images/logo.png" />
+        </Head>
+        <Body>
+          <Base>
+            <Suspense
+              fallback={
+                <div class="p-5 grid place-items-center">
+                  <Spinner />
+                </div>
+              }
+            >
+              <ErrorBoundary>
+                <AddNewProjectModal />
+                <Routes>
+                  <FileRoutes />
+                </Routes>
+              </ErrorBoundary>
+            </Suspense>
+            <Scripts />
+          </Base>
+        </Body>
+      </Html>
+    </MetaProvider>
   )
 }
 
