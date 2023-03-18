c881e6f2e158df46798d108b50467cb4e4956438
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-03-18 20:49:30 +0530

    fix daisyui config

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  daisyui: {
+    themes: 'forest',
+    logs: false,
+  },
+  content: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '../../packages/ui/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
